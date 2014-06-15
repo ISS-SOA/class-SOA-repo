8bfe564cc1b321b74ee1230da2cb7130744babaf
Soumya Ray <soumya.ray@gmail.com>
2014-06-15 23:30:58 +0800

    Created basic demo/blocks.rb

---
+++ demo/blocks.rb
@@ -0,0 +1,13 @@
+def func(x, y, &operation)
+  if operation
+    result = yield x, y
+  else
+    result = x*y
+  end
+end
+
+func(2.0, 3.0)
+# => 6.0
+
+func(2.0, 3.0) { |a, b| a/b }
+# => 0.666666666
