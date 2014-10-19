024ac07834cacee945bf4ec4676b96d9cd9476cd
Soumya Ray <soumya.ray@gmail.com>
2014-10-19 14:53:22 +0800

    Improved regex.rb

--- demo/regex.rb
+++ demo/regex.rb
@@ -9,33 +9,37 @@ phone2 = 'Use this: +(866)-988-162-185 (my 1st choice number)'
 phone3 = "I don't have a number"
 phone4 = '0988-162-185 (or also: 0988-263-064)'
 
-phone1 =~ /\d/					# where does pattern match?
+puts phone1 =~ /\d/					# where does pattern match
+puts phone1 =~ /-\d+-/      # where is the 3-digit middle code
 
 if (pos = (phone2 =~ /\d/))
-  puts " number at #{pos}"
+  puts "digit at #{pos}"
 end
 
-phone3 =~ /\d/					# return NIL if not match
+phone3 =~ /\d/					# returns NIL if not match
 phone3 !~ /\d/					# boolean: no pattern match?
 
-phone1.gsub(/\D/, '')
-phone1.gsub!(/\D/, '')			# the ! saves change
+phone1.gsub(/\D/, '')     # remove non-digts (substitute with blank '')
 
-phone2.gsub(/\D/, '')			# not quite what we want
+phone2.gsub(/\D/, '')			# remove non-digts
 
-# This checks if your matched portion exists
+# Check if digits of parentheses or dashes exit in set of 3 or more
 phone2 =~ /[\d\-\(\)\+]{3,}/
 
-# This captures just the matched portion
+# `match` captures the matching portion
 phone_match = phone2.match(/[\d\-\(\)\+]{3,}/)
 phone_match.to_s
 phone_match.pre_match
 phone_match.post_match
 
-# Using <String>.scan you can capture multiple matches
+# `match` will only capture the first instance of a match
+phone_match = phone4.match(/[\d\-\(\)\+]{3,}/)
+phone_match.to_s
+
+# Using `<String>.scan` you can capture multiple matches
 multi_match = phone4.scan(/[\d\-\(\)\+]{3,}/)
 multi_match[0]
 multi_match[1]
 multi_match[2]
 
-multi_match.each { |num| puts num.gsub(/\D/, '') }
+multi_match.map { |num| num.gsub(/\D/, '') }
