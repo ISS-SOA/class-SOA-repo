721d7363bb0901cbc9cf4b2ab5a810d5f44c3459
Soumya Ray <soumya.ray@gmail.com>
2014-10-19 15:09:23 +0800

    Cleaned up regex-html_match

--- demo/regex-html_match.rb
+++ demo/regex-html_match.rb
@@ -1,16 +1,17 @@
 
-text = "<!--TOCENIZERGEM\nThis is the text\nto be \nreplaced\nTOCENIZERGEM-->"
+# sample HTML text to be searched
+html = "<!--TOCENIZERGEM\nThis is the text\nto be \nreplaced\nTOCENIZERGEM-->"
 
-newtext = text.gsub(/(.*TOCENIZERGEM\N*\n)(.*)(\n\N*TOCENIZERGEM.*)/m, '\2')\
-
-puts newtext
+# regex example to get text within head and tail markers
+extract = html.gsub(/(.*TOCENIZERGEM\N*\n)(.*)(\n\N*TOCENIZERGEM.*)/m, '\2')
+puts extract
 puts
 
-
+# regex example that substitutes text within head and tail markers
 token = 'TOCENIZERGEM'
 head = "(.*#{token}\N*\n)"
 tail = "(\n\N*#{token}.*)"
-content = 'this is the new content!'
-newtext = text.gsub(/#{head}(.*)#{tail}/m, "\\1#{content}\\3")
 
-puts newtext
+content = 'this is the new body content!'
+newhtml = html.gsub(/#{head}(.*)#{tail}/m, "\\1#{content}\\3")
+puts newhtml
