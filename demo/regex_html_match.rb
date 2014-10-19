8872ba6419f6e2bd2350167cdc91b2097e516e45
Soumya Ray <soumya.ray@gmail.com>
2014-10-20 05:13:48 +0800

    cleaned up regex in regex_html_match

--- demo/regex_html_match.rb
+++ demo/regex_html_match.rb
@@ -1,18 +1,19 @@
 # sample HTML text to be searched
-html = "<!--TOCENIZERGEM\nThis is the text\nto be \nreplaced\nTOCENIZERGEM-->"
+html = "Stuff before\n<!--REPLACE start\nThis is the text\nto be"\
+       " \nreplaced\nend REPLACE-->\nMore stuff after"
 puts html
 puts
 
 # regex example to get text within head and tail markers
-extract = html.gsub(/(.*TOCENIZERGEM\N*\n)(.*)(\n\N*TOCENIZERGEM.*)/m, '\2')
+extract = html.gsub(/(.*REPLACE)(.*)(REPLACE.*)/m, '\2')
 puts extract
 puts
 
 # regex example that substitutes text within head and tail markers
-token = 'TOCENIZERGEM'
-head = "(.*#{token}\N*\n)"
-tail = "(\n\N*#{token}.*)"
+token = 'REPLACE'
+head = "(.*#{token})"
+tail = "(#{token}.*$)"
 
-content = 'this is the new body content!'
-newhtml = html.gsub(/#{head}(.*)#{tail}/m, "\\1#{content}\\3")
+content = ' this is the new text!'
+newhtml = html.gsub(/#{head}(.*)#{tail}/m, "\\1\n#{content}\n\\3")
 puts newhtml
