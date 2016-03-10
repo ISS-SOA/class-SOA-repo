3b391a5defa7ef99f367eca62418c677ecf2056d
Soumya Ray <soumya.ray@gmail.com>
2016-03-10 19:45:46 +0800

    add better watir instructions

--- demo/watir.rb
+++ demo/watir.rb
@@ -1,11 +1,13 @@
 require 'watir-webdriver'
-b = Watir::Browser.new
-b.goto 'bit.ly/watir-webdriver-demo'
-b.text_field(:id => 'entry_0').set 'your name'
-b.select_list(:id => 'entry_1').select 'Ruby'
-selected = b.select_list(:id => 'entry_1').selected? 'Ruby'
+
+browser = Watir::Browser.new
+browser.goto 'bit.ly/watir-webdriver-demo'
+browser.text_field(:id => 'entry_0').set 'your name'
+browser.select_list(:id => 'entry_1').select 'Ruby'
+selected = browser.select_list(:id => 'entry_1').selected? 'Ruby'
 puts "TEST: correct language selected" if selected
-b.button(:name => 'submit').click
-thanks = b.text.include? 'Thank you'
+browser.button(:name => 'submit').click
+thanks = browser.text.include? 'Thank you'
 puts "TEST: user thanked!" if thanks
 
+browser.close
